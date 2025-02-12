d00a331765fc7550fbb5f18e48dcf80aeed84bd2
Shah jalal <jalal.chishti122448@gmail.com>
2025-02-12 05:13:01 -0800

    JS Variables, Data Types and Objects

---
+++ script.js
@@ -0,0 +1,56 @@
+console.log("Hy welcome to JS")
+
+// var a= 5;
+// // var 55a="Ali"; not allowed
+// a=a+2;
+// var b= 9;
+// var c="jalal";
+// // const a1=6;
+// // a1=a1+1;not allowed
+// console.log(a+b)
+// console.log(typeof a, typeof b, typeof c)
+// // make modern projects then used Let ,work as globel variable
+// let a= 5;
+
+let x ="Jalali";
+let y = 22;
+let z = 3.66;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+// a=a+2;
+// let b= 9;
+// let c="jalal";
+// console.log(a+b+99)
+
+// {
+//     let a=66;
+//     console.log(a)
+// }
+// console.log(a)
+// var a= 5;
+
+// a=a+2;
+// var b= 9;
+// var c="jalal";
+// console.log(a+b+99)
+
+// {
+//     var a=66;       //   var is globaly scoped
+//     console.log(a)
+// }
+// console.log(a)
+// premitive data types and objects
+let obj = {
+    name : "jalal",
+    "is-handsome": true,
+    "job code" :5600
+}
+console.log(obj);
+obj.salary = "50crores";
+console.log(obj);
+obj.salary = "1000crores";
+console.log(obj);
